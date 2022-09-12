2e0adefb84b4a6014113940d8ef70747586c988c
Jawad Abbasi <Jawadabbasi42@gmail.com>
2022-09-12 16:51:14 +0500

    Add files via upload

---
+++ Display Woocommerce Related Products After Price Section/code.php
@@ -0,0 +1,6 @@
+<?php 
+function woo_related_products_after_price_section(){
+	echo do_shortcode('[related_products limit="12"]');
+}
+add_action('woocommerce_before_add_to_cart_form','woo_related_products_after_price_section');
+?>
